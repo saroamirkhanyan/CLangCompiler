0ea2ddd541c512953295603bb689bbd353741e5e
Saro Amirkhanyan <saroamirkhanyan@gmail.com>
2021-03-11 11:52:28 +0400

    added TOKEN_TYPES IDENTIFIER,FLOAT,COMMENT, added UncaughtError

--- basic.ts
+++ basic.ts
@@ -1,8 +1,19 @@
 enum TOKEN_TYPE {
   WHITESPACE,
+  IDENTFIER,
   INTEGER,
+  FLOAT,
   STRING,
   OPERATOR,
+  COMMENT,
+}
+
+class UncaughtError {
+  constructor(char: string) {
+    throw new Error(
+      `Uncaught SyntaxError: Invalid or unexpected token ${char}`
+    );
+  }
 }
 
 class Token {
@@ -31,33 +42,77 @@ export class Tokenizer {
           }
           currentToken.text += currChar;
           break;
+        case '.':
+          if (
+            currentToken.type == TOKEN_TYPE.INTEGER ||
+            currentToken.type == TOKEN_TYPE.STRING
+          ) {
+            if (currentToken.type == TOKEN_TYPE.INTEGER) {
+              currentToken.type = TOKEN_TYPE.FLOAT;
+            }
+            currentToken.text += currChar;
+          } else {
+            new UncaughtError(currChar);
+          }
+
+          break;
+        case 'a':
+        case 'b':
+        case 'c':
+          if (currentToken.type == TOKEN_TYPE.WHITESPACE) {
+            currentToken.type = TOKEN_TYPE.IDENTFIER;
+          }
+          if (
+            currentToken.type == TOKEN_TYPE.IDENTFIER ||
+            currentToken.type == TOKEN_TYPE.STRING ||
+            currentToken.type == TOKEN_TYPE.COMMENT
+          ) {
+            currentToken.text += currChar;
+          }
+          break;
         case '{':
         case '}':
         case '(':
         case ')':
         case '-':
+        case '+':
         case ';':
         case ',':
-          if (currentToken.type != TOKEN_TYPE.STRING) {
-            tokens.push(currentToken);
-            currentToken = new Token();
+        case '/':
+          if (
+            currentToken.type == TOKEN_TYPE.STRING ||
+            currentToken.type == TOKEN_TYPE.COMMENT
+          ) {
+            currentToken.text += currChar;
+          } else {
+            if (currentToken.type != TOKEN_TYPE.WHITESPACE) {
+              tokens.push(currentToken);
+              currentToken = new Token();
+            }
             currentToken.type = TOKEN_TYPE.OPERATOR;
             currentToken.text = currChar;
             tokens.push(currentToken);
             currentToken = new Token();
-          } else {
-            currentToken.text += currChar;
           }
           break;
         case ' ':
-        case '\t':
-          tokens.push(currentToken);
-          currentToken = new Token();
+          if (
+            currentToken.type == TOKEN_TYPE.STRING ||
+            currentToken.type == TOKEN_TYPE.COMMENT
+          ) {
+            currentToken.text += currChar;
+          } else if (currentToken.type != TOKEN_TYPE.WHITESPACE) {
+            tokens.push(currentToken);
+            currentToken = new Token();
+          }
           break;
-        case '\r':
         case '\n':
-          tokens.push(currentToken);
-          currentToken = new Token();
+          if (currentToken.type != TOKEN_TYPE.WHITESPACE) {
+            if (currentToken.type != TOKEN_TYPE.COMMENT) {
+              tokens.push(currentToken);
+            }
+            currentToken = new Token();
+          }
           break;
         case '"':
           if (currentToken.type == TOKEN_TYPE.WHITESPACE) {
@@ -67,9 +122,21 @@ export class Tokenizer {
             currentToken = new Token();
           }
           break;
+        case '#':
+          if (currentToken.type != TOKEN_TYPE.WHITESPACE) {
+            tokens.push(currentToken);
+          }
+          currentToken = new Token();
+          currentToken.type = TOKEN_TYPE.COMMENT;
+          break;
+        default:
+          new UncaughtError(currChar);
       }
     }
-    if (currentToken.type != TOKEN_TYPE.WHITESPACE) {
+    if (
+      currentToken.type != TOKEN_TYPE.WHITESPACE &&
+      currentToken.type != TOKEN_TYPE.COMMENT
+    ) {
       tokens.push(currentToken);
     }
     return tokens;
